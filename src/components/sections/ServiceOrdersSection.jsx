97067f52f5a5b469bd222c3568241d93f8581e17
yord-96 <yord-96@users.noreply.github.com>
2026-08-19 12:09:45 -0400

    Ajusta anchos de columnas en ordenes

--- src/components/sections/ServiceOrdersSection.jsx
+++ src/components/sections/ServiceOrdersSection.jsx
@@ -10498,13 +10498,13 @@ function ServiceOrdersSection({
                   <col style={{ width: '11%' }} />
                   <col style={{ width: '10%' }} />
                   <col style={{ width: '14%' }} />
-                  <col style={{ width: '9%' }} />
-                  <col style={{ width: '5%' }} />
+                  <col style={{ width: '11%' }} />
+                  <col style={{ width: '6%' }} />
                   <col style={{ width: '8%' }} />
                   <col style={{ width: '7%' }} />
                   <col style={{ width: '7%' }} />
-                  <col style={{ width: '5%' }} />
-                  <col style={{ width: '8%' }} />
+                  <col style={{ width: '4%' }} />
+                  <col style={{ width: '6%' }} />
                 </colgroup>
                 <thead>
                   <tr>
